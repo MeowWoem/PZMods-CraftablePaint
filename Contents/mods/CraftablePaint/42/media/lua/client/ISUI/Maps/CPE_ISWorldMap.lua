c392deb9edc57a4497bf5047a428f4ed1254fe33
Amenophis <l.stoller@outlook.fr>
2026-08-17 10:48:38 +0200

    Block the show world map if the player is temporary blind

--- Contents/mods/CraftablePaint/42/media/lua/client/ISUI/Maps/CPE_ISWorldMap.lua
+++ Contents/mods/CraftablePaint/42/media/lua/client/ISUI/Maps/CPE_ISWorldMap.lua
@@ -1,9 +1,12 @@
 require "ISUI/Maps/ISWorldMap";
 
-local old_ISWorldMap_IsAllowed = ISWorldMap.IsAllowed;
+local old_ISWorldMap_ShowWorldMap = ISWorldMap.ShowWorldMap;
 
--- function ISWorldMap.IsAllowed()
--- 	if(not old_ISWorldMap_IsAllowed()) then return false; end
---     --return CPETemporaryBlindness.getInstance():canReadMap();
---     return true;
--- end
+function ISWorldMap.ShowWorldMap(playerNum, centerX, centerY, zoom)
+	local instance = CPETemporaryBlindnessClient.getInstanceForPlayer(nil, playerNum);
+    if(not instance:canReadMap()) then
+        CPEUtils.addBadText(instance.player, getText("IGUI_CPE_CantReadMap"));
+        return;
+    end
+    old_ISWorldMap_ShowWorldMap(self, playerNum, centerX, centerY, zoom)
+end
