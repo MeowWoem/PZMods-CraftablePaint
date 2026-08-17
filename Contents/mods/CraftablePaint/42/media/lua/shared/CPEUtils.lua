cc863d2864ef2f459b63dd644d99b9916c13cdf8
Amenophis <l.stoller@outlook.fr>
2026-08-17 10:51:21 +0200

    Bad habit from JS removed lol

--- Contents/mods/CraftablePaint/42/media/lua/shared/CPEUtils.lua
+++ Contents/mods/CraftablePaint/42/media/lua/shared/CPEUtils.lua
@@ -2,7 +2,7 @@ CPEUtils = CPEUtils or {};
 
 local badColor = getCore():getBadHighlitedColor();
 
-function CPEUtils.addBadText(character, message, duration) {
+function CPEUtils.addBadText(character, message, duration)
     duration = duration or 400;
     character:setHaloNote(message, badColor:getR() * 255, badColor:getG() * 255, badColor:getB() * 255, duration);
-}
+end
