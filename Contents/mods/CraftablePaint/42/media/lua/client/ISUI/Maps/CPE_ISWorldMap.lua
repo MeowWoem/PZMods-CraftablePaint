856f70583468b246377eb26c95483e38a2da51de
Amenophis <l.stoller@outlook.fr>
2026-08-18 23:47:08 +0200

    Fix the monkey-patch

--- Contents/mods/CraftablePaint/42/media/lua/client/ISUI/Maps/CPE_ISWorldMap.lua
+++ Contents/mods/CraftablePaint/42/media/lua/client/ISUI/Maps/CPE_ISWorldMap.lua
@@ -2,11 +2,20 @@ require "ISUI/Maps/ISWorldMap";
 
 local old_ISWorldMap_ShowWorldMap = ISWorldMap.ShowWorldMap;
 
-function ISWorldMap:ShowWorldMap(playerNum, centerX, centerY, zoom)
+function ISWorldMap.ShowWorldMap(playerNum, centerX, centerY, zoom)
+
+    print("_______________________")
+    print("_______________________")
+    print("_______________________")
+    print(playerNum)
+    print("_______________________")
+    print("_______________________")
+    print("_______________________")
+
 	local instance = CPETemporaryBlindnessClient.getInstanceForPlayer(nil, playerNum);
-    if(not instance:canReadMap()) then
+    if(instance and not instance:canReadMap()) then
         CPEUtils.addBadText(instance.player, getText("IGUI_CPE_CantReadMap"));
         return;
     end
-    old_ISWorldMap_ShowWorldMap(self, playerNum, centerX, centerY, zoom)
+    old_ISWorldMap_ShowWorldMap(playerNum, centerX, centerY, zoom)
 end
