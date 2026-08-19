b036c6814b8d80400d7458a0eb506f55bbb8b36a
Amenophis <l.stoller@outlook.fr>
2026-08-20 00:11:35 +0200

    Remove debug prints

--- Contents/mods/CraftablePaint/42/media/lua/client/ISUI/Maps/CPE_ISWorldMap.lua
+++ Contents/mods/CraftablePaint/42/media/lua/client/ISUI/Maps/CPE_ISWorldMap.lua
@@ -3,15 +3,6 @@ require "ISUI/Maps/ISWorldMap";
 local old_ISWorldMap_ShowWorldMap = ISWorldMap.ShowWorldMap;
 
 function ISWorldMap.ShowWorldMap(playerNum, centerX, centerY, zoom)
-
-    print("_______________________")
-    print("_______________________")
-    print("_______________________")
-    print(playerNum)
-    print("_______________________")
-    print("_______________________")
-    print("_______________________")
-
 	local instance = CPETemporaryBlindnessClient.getInstanceForPlayer(nil, playerNum);
     if(instance and not instance:canReadMap()) then
         CPEUtils.addBadText(instance.player, getText("IGUI_CPE_CantReadMap"));
